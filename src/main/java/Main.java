dcd209750aa8bdf4bdb7c151c2a0d33d961f1849
IliaBu <117571487+IliaBu@users.noreply.github.com>
2023-10-14 01:38:54 +0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args){
+
+        Controller controller = new Controller();
+
+        int[] array1 = controller.createArray();
+        double averageArray1 = controller.averageArray(array1);
+
+        int[] array2 = controller.createArray();
+        double averageArray2 = controller.averageArray(array2);
+
+        controller.compareArray(averageArray1, averageArray2);
+    }
 }
